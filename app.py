32381dfce829c7f54dd6acf76cade002743ef917
mark-joyce <markjoyce25ps@gmail.com>
2020-03-26 09:51:02 +0000

    Added HTML

--- app.py
+++ app.py
@@ -5,15 +5,15 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb+srv://markjoyce:iWnm5L2OjYIUiUd4@myfirstcluster-xd4fg.mongodb.net/test?retryWrites=true&w=majority')
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb+srv://markjoyce:iWnm5L2OjYIUiUd4@myfirstcluster-xd4fg.mongodb.net/task_manager?retryWrites=true&w=majority')
 
 mongo = PyMongo(app)
-
 
 @app.route('/')
 @app.route('/get_tasks')
 def get_tasks():
-    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+    return render_template("tasks.html", 
+                           tasks=mongo.db.tasks.find())
 
 
 @app.route('/add_task')
@@ -22,6 +22,13 @@ def add_task():
                            categories=mongo.db.categories.find())
 
 
+@app.route('/insert_task', methods=['POST'])
+def insert_task():
+    tasks = mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_tasks'))
+    
+    
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
